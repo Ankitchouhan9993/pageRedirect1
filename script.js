94e55d9eb09f0fdae77333b73e53923771e67c6a
Ankit Chouhan <78790534+Ankitchouhan9993@users.noreply.github.com>
2022-02-12 14:50:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var pathObj = new Array("page1.htm", "page2.htm", "page3.htm", "page4.htm", "page5.htm", "page6.htm");
 const num = Math.floor(Math.random() * pathObj.length);
-window.location.replace("www.ankitchouhan9993.github.io/pageRedirect1/" + pathObj[num]);
+window.location.replace(pathObj[num]);
