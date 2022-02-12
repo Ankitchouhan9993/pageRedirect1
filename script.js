16f8991469374ed66092fe156c30d9c452681da0
Ankit Chouhan <78790534+Ankitchouhan9993@users.noreply.github.com>
2022-02-12 14:29:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var pathObj = new Array("page1.htm", "page2.htm", "page3.htm", "page4.htm", "page5.htm", "page6.htm");
+const num = Math.floor(Math.random() * pathObj.length);
+window.location.replace("C:/Users/ankit/OneDrive/Desktop/page redirect/" + pathObj[num]);
